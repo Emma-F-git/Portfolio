d1c03a0bb70754c02075e63ef8f0fa42269bd1eb
Emma-F-git <emma.fortat@yahoo.fr>
2025-06-25 08:49:06 +0200

    Ajout d'un lien linkedin dans le footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,28 @@
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "../styles/footer.scss";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} Emma Fortat – Tous droits réservés</p>
       <div className="footer-links">
         <a
           href="https://github.com/Emma-F-git"
           target="_blank"
-          rel="noopener noreferrer"
           aria-label="GitHub"
           className="github-icon"
         >
-          <FaGithub size={24} />
+          <FaGithub size={26} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/emma-f-1305a41b5/"
+          target="_blank"
+          aria-label="linkedin"
+          className="linkedin-icon"
+        >
+          <FaLinkedin size={26} />
         </a>
       </div>
+      <p>© {new Date().getFullYear()} Emma Fortat – Tous droits réservés</p>
     </footer>
   );
 }
