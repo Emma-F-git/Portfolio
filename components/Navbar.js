d07976b01a7b8dc7f630bece8972139ef66e747b
Emma-F-git <emma.fortat@yahoo.fr>
2025-07-01 08:37:34 +0200

    Ajout d'un menu hamburger pour les liens de la NavBar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,49 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import "../styles/navbar.scss";
 import "../styles/globals.scss";
 import "../styles/tailwind.css";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="mynavbar">
       <Link href="/">
         <img src="/images/Logo.webp" alt="Logo EF" className="logo" />
       </Link>
-      <div className="nav-links">
-        <Link href="/" className="nav-link">
+
+      <button
+        className="menu-toggle md:hidden"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Menu"
+      >
+        ☰
+      </button>
+
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <Link href="/" className="nav-link" onClick={() => setMenuOpen(false)}>
           Accueil
         </Link>
-        <Link href="/#a-propos" className="nav-link">
+        <Link
+          href="/#a-propos"
+          className="nav-link"
+          onClick={() => setMenuOpen(false)}
+        >
           Mon profil
         </Link>
-        <Link href="/#projets" className="nav-link">
+        <Link
+          href="/#projets"
+          className="nav-link"
+          onClick={() => setMenuOpen(false)}
+        >
           Mes projets
         </Link>
-        <Link href="/contact" className="nav-link">
+        <Link
+          href="/contact"
+          className="nav-link"
+          onClick={() => setMenuOpen(false)}
+        >
           Contact
         </Link>
       </div>
